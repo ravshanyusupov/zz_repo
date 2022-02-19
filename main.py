4bd35aa17a5623ec061cf368d3e180bc187f7760
ravshan_yusupov <yusupovravshan001@gmail.com>
2022-02-19 22:32:23 +0500

    first deploying 53th

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ import django
 django.setup()
 
 from db.models import Register
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, \
+from telegram import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, \
     InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, \
     ConversationHandler, MessageHandler, Filters, CallbackQueryHandler
 
-Last_name_handler, First_name_handler, Phone_number_handler, Courses_handler, Time_handler, Get_all_handler = range(6)
-PORT = int(os.environ.get('PORT', 5000))
+Region_handler, First_name_handler, Phone_number_handler, Courses_handler, Time_handler, Response_handler, End_handler = range(7)
 
 
 def start(update, context):
-    update.message.reply_text('Botga xush kelibsiz.\n'
-                              'Ro\'yxatdan o\'tish uchun \'/boshlash\' ni bosing.')
+    update.message.reply_text(
+        'Assalomu aleykum! Yoshlar ishlari Agentligi Buxoro viloyat boshqarmasi IT markazi botiga xush kelibsiz!\n'
+        'Ro\'yxatdan o\'tish uchun \'/boshlash\' ni bosing.')
 
 
 def boshlash(update, context):
-    update.message.reply_text('Ismingizni kiriting.')
+    update.message.reply_text('Ism familiyangizni toʻliq kiriting!')
     return First_name_handler
 
 
@@ -37,24 +37,6 @@ def first_name_handler(update, context):
         context.chat_data.update({
             'first_name': update.message.text
         })
-        update.message.reply_text('Familiyangizni kiriting.')
-        return Last_name_handler
-
-
-def first_name_resend_handler(update, context):
-    update.message.reply_text('Xato kiritildi. Qaytadan urinib ko\'ring.')
-    return Last_name_handler
-
-
-def last_name_handler(update, context):
-    last = update.message.text
-    if last == '/start' or type(last) == int or len(last) < 5:
-        update.message.reply_html('<b>Familiya xato kiritildi.\n'
-                                  'Qaytadan urinib ko\'ring.</b>')
-    else:
-        context.chat_data.update({
-            'last_name': update.message.text
-        })
         update.message.reply_text('Telefon nomeringizni kiritish uchun pastdagi tugmani bosing.',
                                   reply_markup=ReplyKeyboardMarkup([[KeyboardButton('Telefon raqam yuborish',
                                                                                     request_contact=True)]],
@@ -63,8 +45,8 @@ def last_name_handler(update, context):
         return Phone_number_handler
 
 
-def last_name_resend_handler(update, context):
-    update.message.reply_text('Xato nomer kiritildi. Ro\'g\'ri nomer kiriting yoki tugmani bosing.',
+def first_name_resend_handler(update, context):
+    update.message.reply_text('Telefon nomeringizni kiritish uchun pastdagi tugmani bosing.',
                               reply_markup=ReplyKeyboardMarkup([[KeyboardButton('Telefon raqam yuborish',
                                                                                 request_contact=True)]],
                                                                resize_keyboard=True,
@@ -72,13 +54,34 @@ def last_name_resend_handler(update, context):
     return Phone_number_handler
 
 
+# def last_name_handler(update, context):
+#     last = update.message.text
+#     if last == '/start' or type(last) == int or len(last) < 5:
+#         update.message.reply_html('<b>Familiya xato kiritildi.\n'
+#                                   'Qaytadan urinib ko\'ring.</b>')
+#     else:
+#         context.chat_data.update({
+#             'last_name': update.message.text
+#         })
+#
+#         return Phone_number_handler
+
+
+# def last_name_resend_handler(update, context):
+#     update.message.reply_text('Xato nomer kiritildi. Ro\'g\'ri nomer kiriting yoki tugmani bosing.',
+#                               reply_markup=ReplyKeyboardMarkup([[KeyboardButton('Telefon raqam yuborish',
+#                                                                                 request_contact=True)]],
+#                                                                resize_keyboard=True,
+#                                                                one_time_keyboard=True))
+#     return Phone_number_handler
+
+
 def phone_contact_handler(update, context):
     contact = update.message.contact
     context.chat_data.update({
-        'phone_number': '+' + contact.phone_number
+        'phone_number': contact.phone_number
     })
-    update.message.reply_text('Kursni tanlang.')
-    update.message.reply_text('Bizda mavjud b\'lgan kurslar.',
+    update.message.reply_text('Mavjud kurslarimizdan birini tanlang!',
                               reply_markup=ReplyKeyboardMarkup([
                                   [KeyboardButton('Web dasturlash')],
                                   [KeyboardButton('Mobil dasturlash')],
@@ -109,7 +112,7 @@ def courses_handler(update, context):
                 InlineKeyboardButton(text='16:00-18:00', callback_data='16:00-18:00'),
             ]
         ]
-        update.message.reply_text('Siz uchun qulay vaqtni tanlang.',
+        update.message.reply_text('Oʻzingizga qulay vaqtni tanlang!',
                                   reply_markup=InlineKeyboardMarkup(keyboard))
         return Time_handler
     else:
@@ -138,16 +141,61 @@ def time_handler(update, context):
     context.chat_data.update({
         'time': a.data
     })
+    a.message.delete()
+    # print(context.chat_data)
+    # _db = context.chat_data
+    # Register.objects.create(
+    #     ism=_db['first_name'][0:255],
+    #     familya=_db['last_name'][0:255],
+    #     telefon_nomer=_db['phone_number'][0:255],
+    #     kurs=_db['course'][0:255],
+    #     vaqt=_db['time'][0:255],
+    # )
+    a.message.reply_text(text='Siz qaysi tuman yoki shahardansiz?',
+                              reply_markup=ReplyKeyboardMarkup([
+                                  [KeyboardButton('Buxoro shahar'),
+                                   KeyboardButton('Buxoro tuman')],
+                                  [KeyboardButton('Olot'),
+                                   KeyboardButton('Galaosiyo')],
+                                  [KeyboardButton('Gʻijduvon'),
+                                   KeyboardButton('Jondor')],
+                                  [KeyboardButton('Kogon shahar'),
+                                   KeyboardButton('Kogon tuman')],
+                                  [KeyboardButton('Qorakoʻl'),
+                                   KeyboardButton('Qorovulbozor')],
+                                  [KeyboardButton('Yangibozor'),
+                                   KeyboardButton('Romitan')],
+                                  [KeyboardButton('Shofirkon'),
+                                   KeyboardButton('Vobkent')],
+                              ], resize_keyboard=True, one_time_keyboard=True
+                              ))
+    return Region_handler
+
+
+def region_handler(update, context):
+    region = update.message.text
+    # if region == ''
+    context.chat_data.update({
+        'region': region
+    })
+    keyboard = [
+            InlineKeyboardButton(text='Ha', callback_data='Ha'),
+            InlineKeyboardButton(text='Yoq', callback_data='Yoq'),
+        ],
+    update.message.reply_text('Ushbu loyiha boʻyicha oʻtiladigan bepul darslar Buxoro shahridagi Yoshlar Ishlar Agentligi binosida boʻlib oʻtadi. Siz agentlik binosiga kelib, darslarga offlayn tarzda qatnasha olasizmi?',
+                              reply_markup=InlineKeyboardMarkup(keyboard))
+    return End_handler
+
+
+def true_false_handler(update, context):
+    response = update.callback_query
+    context.chat_data.update({
+        'javob': response.data
+    })
     print(context.chat_data)
-    _db = context.chat_data
-    Register.objects.create(
-        ism=_db['first_name'][0:255],
-        familya=_db['last_name'][0:255],
-        telefon_nomer=_db['phone_number'][0:255],
-        kurs=_db['course'][0:255],
-        vaqt=_db['time'][0:255],
-    )
-    a.edit_message_text('Ro\'yxatdan o\'tdingiz va tez orada admistratorlar siz bilan bog\'lanishadi.')
+    # if update.callback_query.data == ''
+    response.edit_message_text(text=
+        'Tabriklaymiz, siz muvaffaqqiyatli roʻyhatdan oʻtdingiz. Tez orada administratorlarimiz siz bilan bog’lanishadi!')
 
 
 def help(update, context):
@@ -173,13 +221,9 @@ updater.dispatcher.add_handler(ConversationHandler(
             MessageHandler(Filters.text, first_name_handler),
             MessageHandler(Filters.all, start)
         ],
-        Last_name_handler: [
-            MessageHandler(Filters.text, last_name_handler),
-            MessageHandler(Filters.all, first_name_resend_handler)
-        ],
         Phone_number_handler: [
             MessageHandler(Filters.contact, phone_contact_handler),
-            MessageHandler(Filters.all, last_name_resend_handler),
+            MessageHandler(Filters.all, first_name_resend_handler),
         ],
         Courses_handler: [
             MessageHandler(Filters.text, courses_handler),
@@ -188,6 +232,18 @@ updater.dispatcher.add_handler(ConversationHandler(
         Time_handler: [
             CallbackQueryHandler(time_handler),
             CallbackQueryHandler(time_handler),
+        ],
+        Region_handler: [
+            MessageHandler(Filters.text, region_handler),
+            MessageHandler(Filters.all, time_handler)
+        ],
+        Response_handler: [
+            CallbackQueryHandler(true_false_handler),
+            CallbackQueryHandler(true_false_handler),
+        ],
+        End_handler: [
+            CallbackQueryHandler(true_false_handler),
+            # MessageHandler(Filters.all, true_false_handler)
         ]
     },
     fallbacks=[]
